36fdaef3c7f4d43088979aaac6c3010cb99abe1a
louckazdenekjr <louckazdnekejr@gmail.com>
2023-04-21 23:05:59 +0200

    added terminal class

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 #----------------------------------------
 # CONFIG VALUES
 config = {
-	"address": "D6:27:82:98:07:D1", # -> "none"
+	"address": "none", # -> "D6:27:82:98:07:D1"
 	"cell_series": "15",
 	"cell_min": "2.7",
 	"cell_max": "4.2",
